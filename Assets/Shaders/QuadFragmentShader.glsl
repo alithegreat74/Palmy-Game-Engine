e22bfecf54e5a0ff422f864ecdcab1e7100f6c18
AlirezaMohammadi <am1514168021@gmail.com>
2025-03-25 21:30:42 +0330

    #29 - Fixed textures rendering flipped horizontally

--- Assets/Shaders/QuadFragmentShader.glsl
+++ Assets/Shaders/QuadFragmentShader.glsl
@@ -5,5 +5,5 @@ uniform sampler2D uTexture;
 uniform vec4 uColor;
 void main()
 {
-	FragColor = vec4(texture(uTexture,TextureCoordinates)) * uColor;
+	FragColor = vec4(texture(uTexture,vec2(1-TextureCoordinates.x,TextureCoordinates.y))) * uColor;
 }
